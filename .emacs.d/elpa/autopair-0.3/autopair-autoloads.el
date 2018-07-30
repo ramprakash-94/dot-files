9dcf4f328067a41d9922ca4ba5a821c8acb2858b
Ram Prakash Arivu Chelvan <reram@channing.harvard.edu>
2018-07-30 17:23:35 -0400

    New emacs config with evil

---
+++ .emacs.d/elpa/autopair-0.3/autopair-autoloads.el
@@ -0,0 +1,22 @@
+;;; autopair-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "autopair" "autopair.el" (0 0 0 0))
+;;; Generated autoloads from autopair.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "autopair" '("autopair-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; autopair-autoloads.el ends here
